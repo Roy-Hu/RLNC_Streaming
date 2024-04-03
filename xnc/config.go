13bafe44163351109c703ea9637a5938a3358d24
Roy-Hu <r74777477@gmail.com>
2024-04-02 20:47:23 -0500

    add multipath enviroment

--- xnc/config.go
+++ xnc/config.go
@@ -2,6 +2,6 @@ package xnc
 
 var CHUNKSIZE int = 1 << 14
 
-var clientaddr string = "localhost:4242"
+var clientaddr string = "0.0.0.0:4242"
 
-var serveraddr string = "localhost:4242"
+var serveraddr string = "10.0.2.2:4242"
